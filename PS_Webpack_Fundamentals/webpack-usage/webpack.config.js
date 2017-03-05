8dd071efa9e13cf4c1b3d06847a789adc37edbb8
Jerry Hu <hh201020@gmail.com>
2017-03-05 14:53:56 -0500

    Organizing Files and Folders by path

--- PS_Webpack_Fundamentals/webpack-usage/webpack.config.js
+++ PS_Webpack_Fundamentals/webpack-usage/webpack.config.js
@@ -1,18 +1,19 @@
+var path = require('path');
+
 module.exports = {
-	entry: ["./utils.js", "./app.js"],
+	context: path.resolve('js'),
+	entry: ["./utils", "./app"],
 	output: {
+		path: path.resolve('build/js/'),
+		publicPath: '/public/assets/js/',
 		filename: "bundle.js"
 	},
-	watch: true,
 
-	module: {		
-		preLoaders: [
-			{
-				test: /\.js$/,
-				exclude: 'node_modules',
-				loader: 'jshint-loader'
-			}
-		],
+	devServer: {
+		contentBase: 'public'
+	},
+
+	module: {
 		loaders: [
 			{
 				test: /\.es6$/,
@@ -25,5 +26,4 @@ module.exports = {
 	resolve: {
 		extensions: ['', '.js', '.es6']
 	}
-
 }
