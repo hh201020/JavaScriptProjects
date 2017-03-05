ee971a6d5a326009849ac4c914414837757f6649
Jerry Hu <hh201020@gmail.com>
2017-03-05 15:47:42 -0500

    Using less npm install less-loader --save-dev npm install less --save-dev

--- PS_Webpack_Fundamentals/webpack-usage/js/app.js
+++ PS_Webpack_Fundamentals/webpack-usage/js/app.js
@@ -1,7 +1,7 @@
 console.log('App loaded');
 
 require('../css/bootstrap.css');
-require('../css/app.scss');
+require('../css/app.less');
 
 
 
--- PS_Webpack_Fundamentals/webpack-usage/webpack.config.js
+++ PS_Webpack_Fundamentals/webpack-usage/webpack.config.js
@@ -1,14 +1,14 @@
 var path = require('path');
 
+
 module.exports = {
 	context: path.resolve('js'),
 	entry: ["./app"],
 	output: {
-		path: path.resolve('build/js/'),
-		publicPath: '/public/assets/js/',
+		path: path.resolve('build/'),
+		publicPath: '/public/assets/',
 		filename: "bundle.js"
 	},
-
 	devServer: {
 		contentBase: 'public'
 	},
@@ -21,9 +21,9 @@ module.exports = {
 				loader: "style-loader!css-loader"
 			},
 			{
-				test: /\.scss$/,
+				test: /\.less$/,
 				exclude: /node_modules/,
-				loader: "style-loader!css-loader!sass-loader"
+				loader: "style-loader!css-loader!less-loader"
 			}
 		]
 	},
