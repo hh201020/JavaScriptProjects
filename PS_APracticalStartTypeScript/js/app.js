db9606d5d1bc53f03367d9e7be7726ff1e970eb2
Jerry Hu <hh201020@gmail.com>
2017-03-07 13:14:18 -0500

    Let / Arrays / Constructors / Static Members / Enums / Loops

--- PS_APracticalStartTypeScript/js/app.js
+++ PS_APracticalStartTypeScript/js/app.js
@@ -54,20 +54,94 @@ var Dollar = (function (_super) {
     };
     return Dollar;
 }(Coin));
+var SodaCategory = (function () {
+    function SodaCategory() {
+        this.name = "Soda";
+    }
+    SodaCategory.prototype.getImageUrl = function () {
+        return "img/SodaCan.png";
+    };
+    return SodaCategory;
+}());
+/// <reference path="productCategory.ts" />
+var CocaCola = (function () {
+    function CocaCola() {
+        this.name = "Coca-Cola";
+        this.price = 2.30;
+        this.category = new SodaCategory();
+    }
+    return CocaCola;
+}());
+/// <reference path="product.ts" />
+var productFactory = (function () {
+    function productFactory() {
+    }
+    productFactory.GetProduct = function () {
+        return new CocaCola();
+    };
+    return productFactory;
+}());
 /// <reference path="./coin.ts" />
+/// <reference path="product.ts" />
+/// <reference path="productFactory.ts" />
+var VendingMachineSize;
+(function (VendingMachineSize) {
+    VendingMachineSize[VendingMachineSize["small"] = 6] = "small";
+    VendingMachineSize[VendingMachineSize["medium"] = 9] = "medium";
+    VendingMachineSize[VendingMachineSize["large"] = 12] = "large";
+})(VendingMachineSize || (VendingMachineSize = {}));
+var Cell = (function () {
+    function Cell(product) {
+        this.product = product;
+        this.stock = ko.observable(3);
+        this.sold = ko.observable(false);
+    }
+    return Cell;
+}());
 var vendingMachine = (function () {
     function vendingMachine() {
         var _this = this;
         this.paid = ko.observable(0);
+        this.selectedCell = ko.observable(new Cell(new CocaCola()));
         this.acceptedCoins = [new Quarter()];
+        this.cells = ko.observableArray([]);
+        this.canPay = ko.pureComputed(function () { return _this.paid() - _this.selectedCell().product.price >= 0; });
+        this.select = function (cell) {
+            cell.sold(false);
+            _this.selectedCell(cell);
+        };
         this.acceptCoin = function (coin) {
             var oldTotal = _this.paid();
             _this.paid(oldTotal + coin.value);
         };
+        this.pay = function () {
+            if (_this.selectedCell().stock() < 1) {
+                alert("I'm sorry, we're out of them!");
+                return;
+            }
+            var currentPayed = _this.paid();
+            _this.paid(Math.round(((currentPayed - _this.selectedCell().product.price) * 100)) / 100);
+            var currentStock = _this.selectedCell().stock();
+            _this.selectedCell().stock(currentStock - 1);
+            _this.selectedCell().sold(true);
+        };
     }
+    Object.defineProperty(vendingMachine.prototype, "size", {
+        set: function (givenSize) {
+            this.cells([]);
+            for (var index = 0; index < givenSize; index++) {
+                var product = productFactory.GetProduct();
+                this.cells.push(new Cell(product));
+            }
+            ;
+        },
+        enumerable: true,
+        configurable: true
+    });
     return vendingMachine;
 }());
 /// <reference path="vendingMachine.ts" />
 var machine = new vendingMachine();
+machine.size = VendingMachineSize.medium;
 ko.applyBindings(machine);
 //# sourceMappingURL=app.js.map
--- PS_APracticalStartTypeScript/ts/bootstrapper.ts
+++ PS_APracticalStartTypeScript/ts/bootstrapper.ts
@@ -1,5 +1,6 @@
 /// <reference path="vendingMachine.ts" />
 
 let machine = new vendingMachine();
+machine.size = VendingMachineSize.medium;
 
 ko.applyBindings(machine);
